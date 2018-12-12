239bebae5f3c4ebccc88793c96e0bc10dbe1c20d
Vincent Golle <vincent.golle@gmail.com>
2018-12-12 16:09:50 +0100

    Added different init for both memories

---
+++ libraries/SST/nor_defines.h
@@ -0,0 +1,18 @@
+/*
+ * nor_defines.h
+ *
+ *  Created on: 12 déc. 2018
+ *      Author: Vincent
+ */
+
+#ifndef LIBRARIES_SST_NOR_DEFINES_H_
+#define LIBRARIES_SST_NOR_DEFINES_H_
+
+
+#define QSPI_STD_CMD_WRSR   0x01
+#define QSPI_STD_CMD_RSTEN  0x66
+#define QSPI_STD_CMD_RST    0x99
+
+
+
+#endif /* LIBRARIES_SST_NOR_DEFINES_H_ */
--- libraries/SST/sst26.c
+++ libraries/SST/sst26.c
@@ -1,10 +1,17 @@
 
+#if !defined (PROTO_V10)
+
+#include <stdbool.h>
+#include "boards.h"
+#include "nrfx_qspi.h"
+#include "nor_defines.h"
 #include "segger_wrapper.h"
 #include "nrf_serial_flash_params.h"
 
+
 static const nrf_serial_flash_params_t m_sflash_params[] = {
-    {    /*MT25*/
-        .read_id = { 0x20, 0xBA, 0x18 },
+    {    /*SST26*/
+        .read_id = { 0xBF, 0x26, 0x42 },
         .capabilities = 0x00,
         .size = 128 * 1024 * 1024 / 8,
         .erase_size = 4 * 1024,
@@ -29,3 +36,58 @@ nrf_serial_flash_params_t const * nrf_serial_flash_params_get(const uint8_t * p_
 
     return NULL;
 }
+
+bool configure_memory()
+{
+	uint32_t err_code;
+	nrf_qspi_cinstr_conf_t cinstr_cfg = {
+			.opcode    = QSPI_STD_CMD_RSTEN,
+			.length    = NRF_QSPI_CINSTR_LEN_1B,
+			.io2_level = true,
+			.io3_level = true,
+			.wipwait   = true,
+			.wren      = true
+	};
+
+	// Send reset enable
+	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
+	APP_ERROR_CHECK(err_code);
+
+	// Send reset command
+	cinstr_cfg.opcode = QSPI_STD_CMD_RST;
+	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
+	APP_ERROR_CHECK(err_code);
+
+	delay_ms(3);
+
+	// Get device ID
+	cinstr_cfg.opcode = 0x9F;
+	cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_4B;
+	uint8_t recv[3] = {0};
+	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, recv);
+	APP_ERROR_CHECK(err_code);
+	LOG_INFO("JEDEC ID: 0x%02X 0x%02X 0x%02X", recv[0], recv[1], recv[2]);
+
+	// write status register
+    cinstr_cfg.opcode = 0x01;
+    uint8_t temporary[2] = {0};
+    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_3B;
+    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, &temporary, NULL);
+    APP_ERROR_CHECK(err_code);
+
+	// Global unlock SST26VF
+	cinstr_cfg.opcode = 0x98;
+	cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_1B;
+	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
+	APP_ERROR_CHECK(err_code);
+
+    // Switch to 4-io-qspi mode
+//    cinstr_cfg.opcode = 0x38;
+//    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_1B;
+//    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
+//    APP_ERROR_CHECK(err_code);
+
+	return recv[0] == 0xBF;
+}
+
+#endif
