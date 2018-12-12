7ee5a78a7646e1ce3076f07835f3dc33374ffefb
Vincent Golle <vincent.golle@gmail.com>
2018-12-12 16:13:10 +0100

    Removed function which was forked

--- source/sd/sd_hal.c
+++ source/sd/sd_hal.c
@@ -6,10 +6,12 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include "boards.h"
 #include "millis.h"
 #include "nrfx_qspi.h"
+#include "nor_defines.h"
 #include "sd_hal.h"
 #include "segger_wrapper.h"
 
@@ -22,98 +24,13 @@ static FATFS fs;
 
 static bool m_is_fat_mounted = false;
 
-#define QSPI_STD_CMD_WRSR   0x01
-#define QSPI_STD_CMD_RSTEN  0x66
-#define QSPI_STD_CMD_RST    0x99
-
 #define QSPI_TEST_DATA_SIZE 32
 static uint8_t m_buffer_tx[QSPI_TEST_DATA_SIZE];
 static uint8_t m_buffer_rx[QSPI_TEST_DATA_SIZE];
 
 static void fatfs_mkfs(void);
 
-static bool configure_memory()
-{
-	uint32_t err_code;
-	uint8_t recv[3];
-    uint8_t conf_reg[2] = {0};
-    uint8_t stat_reg = 0;
-	nrf_qspi_cinstr_conf_t cinstr_cfg = {
-			.opcode    = QSPI_STD_CMD_RSTEN,
-			.length    = NRF_QSPI_CINSTR_LEN_1B,
-			.io2_level = true,
-			.io3_level = true,
-			.wipwait   = true,
-			.wren      = false
-	};
-
-    // Exit XIP & QSPI
-	for (int i=0; i < 6; i++) {
-		cinstr_cfg.opcode = 0xFF;
-		cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_1B;
-		err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
-		APP_ERROR_CHECK(err_code);
-	}
-
-	// Reset QIO
-    cinstr_cfg.opcode = QSPI_STD_CMD_RSTEN;
-    stat_reg = 0xF5;
-    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_2B;
-	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, &stat_reg, NULL);
-	APP_ERROR_CHECK(err_code);
-
-	// Send reset enable
-    cinstr_cfg.opcode = QSPI_STD_CMD_RSTEN;
-    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_1B;
-	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
-	APP_ERROR_CHECK(err_code);
-
-	// Send reset command
-	cinstr_cfg.opcode = QSPI_STD_CMD_RST;
-	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
-	APP_ERROR_CHECK(err_code);
-
-	// Get device ID
-	cinstr_cfg.opcode = 0xAF;
-	cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_4B;
-	err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, recv);
-	APP_ERROR_CHECK(err_code);
-	LOG_INFO("JEDEC ID: 0x%02X 0x%02X 0x%02X", recv[0], recv[1], recv[2]);
-
-	// write status register
-    cinstr_cfg.opcode = 0x01;
-    stat_reg = 0;
-    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_2B;
-    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, &stat_reg, NULL);
-    APP_ERROR_CHECK(err_code);
-
-	// write conf register
-    cinstr_cfg.opcode = 0xB1;
-    conf_reg[1] = 0xFF;
-    conf_reg[0] = 0xFF;
-    conf_reg[0] &= ~(1 << 2);
-    conf_reg[0] &= ~(1 << 3);
-    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_3B;
-    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, &conf_reg, NULL);
-    APP_ERROR_CHECK(err_code);
-
-    LOG_INFO("Write conf register: 0x%02X%02X", conf_reg[1], conf_reg[0]);
-
-	// write enhanced volatile conf. register
-    cinstr_cfg.opcode = 0x61;
-    stat_reg = 0x3F;
-    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_2B;
-    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, &stat_reg, NULL);
-    APP_ERROR_CHECK(err_code);
-
-    // Switch to 4-io-qspi mode
-//    cinstr_cfg.opcode = 0x35;
-//    cinstr_cfg.length = NRF_QSPI_CINSTR_LEN_1B;
-//    err_code = nrfx_qspi_cinstr_xfer(&cinstr_cfg, NULL, NULL);
-//    APP_ERROR_CHECK(err_code);
-
-	return recv[0] == 0x20;
-}
+extern bool configure_memory();
 
 void format_memory() {
 
