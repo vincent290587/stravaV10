0216f71410dcaa863ee2e530822a82f85d3b54f6
vincent290587 <vincent.golle@gmail.com>
2019-01-21 20:22:41 +0100

    New proto: new pins !

--- custom_board_v2.h
+++ custom_board_v2.h
@@ -18,9 +18,9 @@ extern "C" {
 // LEDs definitions
 #define LEDS_NUMBER    1
 
-//#define LED_START      14
-#define LED_1            NRF_GPIO_PIN_MAP(0, 19)
-//#define LED_STOP       14
+#define LED_1            NRF_GPIO_PIN_MAP(1, 3)
+#define LED_START        LED_1
+#define LED_STOP         LED_1
 
 #define LEDS_ACTIVE_STATE 0
 
@@ -36,9 +36,9 @@ extern "C" {
 #define BUTTONS_NUMBER 3
 
 //#define BUTTON_START   17
-#define BUTTON_1       NRF_GPIO_PIN_MAP(0, 17)
-#define BUTTON_2       NRF_GPIO_PIN_MAP(0, 15)
-#define BUTTON_3       NRF_GPIO_PIN_MAP(0, 13)
+#define BUTTON_1       NRF_GPIO_PIN_MAP(0, 14)
+#define BUTTON_2       NRF_GPIO_PIN_MAP(0, 13)
+#define BUTTON_3       NRF_GPIO_PIN_MAP(0, 11)
 //#define BUTTON_STOP    19
 #define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
 
@@ -50,38 +50,38 @@ extern "C" {
 #define BSP_BUTTON_1   BUTTON_2
 #define BSP_BUTTON_2   BUTTON_3
 
-#define SDA_PIN_NUMBER    NRF_GPIO_PIN_MAP(1, 15)
-#define SCL_PIN_NUMBER    NRF_GPIO_PIN_MAP(1, 13)
+#define SDA_PIN_NUMBER    NRF_GPIO_PIN_MAP(0, 26)
+#define SCL_PIN_NUMBER    NRF_GPIO_PIN_MAP(0, 25)
 
-#define TX_PIN_NUMBER     NRF_GPIO_PIN_MAP(1, 9)
-#define RX_PIN_NUMBER     NRF_GPIO_PIN_MAP(0, 12)
+#define TX_PIN_NUMBER     NRF_GPIO_PIN_MAP(0, 3)
+#define RX_PIN_NUMBER     NRF_GPIO_PIN_MAP(1, 14)
 #define CTS_PIN_NUMBER    0xFF
 #define RTS_PIN_NUMBER    0xFF
 #define HWFC              false
 
-#define LS027_MOSI_PIN    NRF_GPIO_PIN_MAP(0, 22)
-#define LS027_SCK_PIN     NRF_GPIO_PIN_MAP(0, 20)
-#define LS027_CS_PIN      NRF_GPIO_PIN_MAP(0, 24)
+#define LS027_MOSI_PIN    NRF_GPIO_PIN_MAP(0, 16)
+#define LS027_SCK_PIN     NRF_GPIO_PIN_MAP(0, 15)
+#define LS027_CS_PIN      NRF_GPIO_PIN_MAP(0, 17)
 
-#define QSPI_MISO_PIN     NRF_GPIO_PIN_MAP(0, 10)
-#define QSPI_MOSI_PIN     NRF_GPIO_PIN_MAP(1, 2)
-#define QSPI_SCK_PIN      NRF_GPIO_PIN_MAP(1, 4)
-#define QSPI_SS_PIN       NRF_GPIO_PIN_MAP(1, 6)
-#define QSPI_IO2_PIN      NRF_GPIO_PIN_MAP(0, 9)
-#define QSPI_IO3_PIN      NRF_GPIO_PIN_MAP(1, 0)
+#define QSPI_MISO_PIN     NRF_GPIO_PIN_MAP(0, 23)
+#define QSPI_MOSI_PIN     NRF_GPIO_PIN_MAP(0, 19)
+#define QSPI_SCK_PIN      NRF_GPIO_PIN_MAP(0, 2)
+#define QSPI_SS_PIN       NRF_GPIO_PIN_MAP(0, 18)
+#define QSPI_IO2_PIN      NRF_GPIO_PIN_MAP(0, 22)
+#define QSPI_IO3_PIN      NRF_GPIO_PIN_MAP(0, 21)
 
-#define FXOS_INT1         NRF_GPIO_PIN_MAP(0, 29)
-#define FXOS_RST          NRF_GPIO_PIN_MAP(0, 2)
+#define FXOS_INT1         NRF_GPIO_PIN_MAP(0, 28)
+#define FXOS_RST          NRF_GPIO_PIN_MAP(0, 27)
 
-#define BCK_PIN           NRF_GPIO_PIN_MAP(1, 1)
+#define BCK_PIN           NRF_GPIO_PIN_MAP(1, 8)
 
-#define GPS_R             NRF_GPIO_PIN_MAP(0, 4)
-#define GPS_S             NRF_GPIO_PIN_MAP(0, 6)
-#define FIX_PIN           NRF_GPIO_PIN_MAP(0, 8)
+#define GPS_R             NRF_GPIO_PIN_MAP(0, 5)
+#define GPS_S             NRF_GPIO_PIN_MAP(1, 15)
+#define FIX_PIN           NRF_GPIO_PIN_MAP(0, 7)
 
-#define KILL_PIN          NRF_GPIO_PIN_MAP(1, 10)
+#define KILL_PIN          NRF_GPIO_PIN_MAP(0, 12)
 
-#define NEO_PIN           NRF_GPIO_PIN_MAP(0, 26)
+#define NEO_PIN           NRF_GPIO_PIN_MAP(1, 13)
 
 
 #ifdef __cplusplus
