41383c5972b62413366b65f3061908e56e322040
Vincent Gollé <vincent.golle@gmail.com>
2019-02-15 10:27:36 +0100

    Update utils.h
    Made functions pure

--- libraries/utils/utils.h
+++ libraries/utils/utils.h
@@ -18,10 +18,11 @@ extern "C" {
 #endif
 
 
-float regFen(float val_, float b1_i, float b1_f, float b2_i, float b2_f);
+float regFen(float val_, float b1_i, float b1_f, float b2_i, float b2_f) __attribute__ ((pure));
 
-float regFenLim(float val_, float b1_i, float b1_f, float b2_i, float b2_f);
+float regFenLim(float val_, float b1_i, float b1_f, float b2_i, float b2_f) __attribute__ ((pure));
 
+static volatile float toRadians(float angle) __attribute__ ((pure));
 static volatile float toRadians(float angle) {
   return M_PI * angle / 180.0;
 }
@@ -31,8 +32,9 @@ static volatile float toRadians(float angle) {
  * distance_between2: 24ms
  * distance_between3: 21ms
  * distance_between4: 40ms
- * 
+ * distance_between: ?
  */
+static inline float distance_between(float lat1, float lon1, float lat2, float lon2) __attribute__ ((pure));
 static inline float distance_between(float lat1, float lon1, float lat2, float lon2) {
     const float two_r = 2. * 6371008.; // meters
 //    const float sdlat = my_sin(toRadians(lat2 - lat1) / 2);
@@ -52,17 +54,17 @@ long unsigned int toBase10 (char *entree);
 
 extern void loggerMsg(const char *msg_);
 
-double radians(double value);
+double radians(double value) __attribute__ ((pure));
 
-double degrees(double value);
+double degrees(double value) __attribute__ ((pure));
 
-double sq(double value);
+double sq(double value) __attribute__ ((pure));
 
-uint32_t get_sec_jour(uint8_t hour_, uint8_t min_, uint8_t sec_);
+uint32_t get_sec_jour(uint8_t hour_, uint8_t min_, uint8_t sec_) __attribute__ ((pure));
 
-float compute2Complement(uint8_t msb, uint8_t lsb);
+float compute2Complement(uint8_t msb, uint8_t lsb) __attribute__ ((pure));
 
-float percentageBatt(float tensionValue, float current);
+float percentageBatt(float tensionValue, float current) __attribute__ ((pure));
 
 void encode_uint16 (uint8_t* dest, uint16_t input);
 
