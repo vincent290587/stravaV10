e6b77e5bdce3624b0a7fdd8084e957869af648e6
Vincent Gollé <vincent@kien.io>
2019-03-13 15:50:45 +0100

    Update parameters.h
    Reverted baro update time

--- source/parameters.h
+++ source/parameters.h
@@ -51,7 +51,7 @@
 
 #define TOT_HEAP_MEM_AVAILABLE         __HEAP_SIZE
 
-#define BARO_REFRESH_PER_MS            168
+#define BARO_REFRESH_PER_MS            333
 
 #define SENSORS_REFRESH_PER_MS         1000
 
