20742dee2d55340b28355c85889092dd01ca0ea0
v.golle <v.golle@isispace.nl>
2020-01-30 13:26:54 +0100

    Ported AP

--- AP/main.c
+++ AP/main.c
@@ -63,6 +63,7 @@
 #include "nrf_sdh_soc.h"
 #include "nrf_sdh_ble.h"
 #include "nrf_ble_gatt.h"
+#include "nrf_ble_qwr.h"
 #include "app_timer.h"
 #include "ble_nus.h"
 #include "app_uart.h"
@@ -76,16 +77,26 @@
 #include "nrf_drv_usbd.h"
 #include "nrf_drv_clock.h"
 #include "nrf_gpio.h"
+#include "nrf_queue.h"
 #include "nrf_delay.h"
 #include "nrf_drv_power.h"
 
 #include "app_error.h"
 #include "app_util.h"
+#include "app_scheduler.h"
 #include "app_usbd_core.h"
 #include "app_usbd.h"
 #include "app_usbd_string_desc.h"
 #include "app_usbd_cdc_acm.h"
 #include "app_usbd_serial_num.h"
+
+
+#define SCHED_MAX_EVENT_DATA_SIZE      250              /**< Maximum size of scheduler events. */
+#ifdef SVCALL_AS_NORMAL_FUNCTION
+#define SCHED_QUEUE_SIZE               20                                           /**< Maximum number of events in the scheduler queue. More is needed in case of Serialization. */
+#else
+#define SCHED_QUEUE_SIZE               20                                           /**< Maximum number of events in the scheduler queue. */
+#endif
 
 #define LED_BLE_NUS_CONN (BSP_BOARD_LED_0)
 #define LED_BLE_NUS_RX   (BSP_BOARD_LED_1)
@@ -157,12 +168,15 @@ APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
 
 #define DEAD_BEEF                       0xDEADBEEF                                  /**< Value used as error code on stack dump. Can be used to identify stack location on stack unwind. */
 
-#define UART_TX_BUF_SIZE                256                                         /**< UART TX buffer size. */
-#define UART_RX_BUF_SIZE                256                                         /**< UART RX buffer size. */
 
+typedef struct {
+	uint8_t p_xfer_str[BLE_NUS_MAX_DATA_LEN];
+	uint16_t length;
+} sNusXfer;
 
 BLE_NUS_DEF(m_nus, NRF_SDH_BLE_TOTAL_LINK_COUNT);                                   /**< BLE NUS service instance. */
 NRF_BLE_GATT_DEF(m_gatt);                                                           /**< GATT module instance. */
+NRF_BLE_QWR_DEF(m_qwr);                                                             /**< GATT module instance. */
 BLE_ADVERTISING_DEF(m_advertising);                                                 /**< Advertising module instance. */
 
 static uint16_t   m_conn_handle          = BLE_CONN_HANDLE_INVALID;                 /**< Handle of the current connection. */
@@ -174,15 +188,20 @@ static ble_uuid_t m_adv_uuids[]          =                                      
 
 // BLE DEFINES END
 
+NRF_QUEUE_DEF(sNusXfer, m_to_usb_queue, 10, NRF_QUEUE_MODE_NO_OVERFLOW);
+NRF_QUEUE_DEF(sNusXfer, m_to_nus_queue, 10, NRF_QUEUE_MODE_NO_OVERFLOW);
+
+static sNusXfer m_to_cdc_data;
+static sNusXfer m_to_nus_data;
+
 static char m_cdc_data_array[BLE_NUS_MAX_DATA_LEN];
 
-static char m_nus_data_array[BLE_NUS_MAX_DATA_LEN];
-
 static volatile bool m_is_xfer_done = true;
-
 static volatile bool m_is_port_open = false;
+static volatile bool m_nus_cts = false;
 
 static bool m_usb_connected = false;
+static bool m_nus_connected = false;
 
 /**
  * Prints a char* to VCOM printf style
@@ -276,6 +295,19 @@ static void gap_params_init(void)
 }
 
 
+/**@brief Function for handling Queued Write Module errors.
+ *
+ * @details A pointer to this function will be passed to each service which may need to inform the
+ *          application about an error.
+ *
+ * @param[in]   nrf_error   Error code containing information about what went wrong.
+ */
+static void nrf_qwr_error_handler(uint32_t nrf_error)
+{
+	APP_ERROR_HANDLER(nrf_error);
+}
+
+
 /**
  * @brief Function for handling the data from the Nordic UART Service.
  *
@@ -292,17 +324,102 @@ static void nus_data_handler(ble_nus_evt_t * p_evt)
 		bsp_board_led_invert(LED_BLE_NUS_RX);
 		NRF_LOG_DEBUG("Received data from BLE NUS. Writing data on CDC ACM.");
 		NRF_LOG_HEXDUMP_DEBUG(p_evt->params.rx_data.p_data, p_evt->params.rx_data.length);
-        memcpy(m_nus_data_array, p_evt->params.rx_data.p_data, p_evt->params.rx_data.length);
 
-        // Send data through CDC ACM
-        ret_code_t ret = app_usbd_cdc_acm_write(&m_app_cdc_acm,
-                                                m_nus_data_array,
-												p_evt->params.rx_data.length);
-        if(ret != NRF_SUCCESS)
-        {
-            NRF_LOG_INFO("CDC ACM unavailable, data received: %s", m_nus_data_array);
-        }
+		if (!nrf_queue_is_full(&m_to_usb_queue)) {
 
+			sNusXfer _xfer;
+
+			memset(&_xfer, 0, sizeof(_xfer));
+
+			// get data
+			_xfer.length = p_evt->params.rx_data.length;
+			memcpy(_xfer.p_xfer_str, p_evt->params.rx_data.p_data, p_evt->params.rx_data.length);
+
+			// store it in a queue
+			ret_code_t err_code = nrf_queue_push(&m_to_usb_queue, &_xfer);
+			APP_ERROR_CHECK(err_code);
+
+		} else {
+
+			NRF_LOG_ERROR("CDC queue full");
+		}
+
+	}
+}
+
+static void cdc_process(void) {
+
+	if (!m_to_cdc_data.length && !nrf_queue_is_empty(&m_to_usb_queue)) {
+
+		// get the next victim
+		ret_code_t err_code = nrf_queue_pop(&m_to_usb_queue, &m_to_cdc_data);
+		APP_ERROR_CHECK(err_code);
+	}
+
+	if (m_is_port_open &&
+			m_to_cdc_data.length) {
+
+		// Send data through CDC ACM
+		ret_code_t ret = app_usbd_cdc_acm_write(&m_app_cdc_acm,
+				m_to_cdc_data.p_xfer_str,
+				m_to_cdc_data.length);
+		if(ret != NRF_SUCCESS)
+		{
+			NRF_LOG_INFO("CDC ACM unavailable");
+		}
+	}
+
+}
+
+static void nus_process(void) {
+
+	if (!m_to_nus_data.length && !nrf_queue_is_empty(&m_to_nus_queue)) {
+
+		// get the next victim
+		ret_code_t err_code = nrf_queue_pop(&m_to_nus_queue, &m_to_nus_data);
+		APP_ERROR_CHECK(err_code);
+	}
+
+	while (m_nus_connected &&
+			m_nus_cts &&
+			m_to_nus_data.length) {
+
+		// Send data to NUS
+		ret_code_t ret = ble_nus_data_send(&m_nus,
+				(uint8_t *) m_to_nus_data.p_xfer_str,
+				&m_to_nus_data.length,
+				m_conn_handle);
+
+		switch (ret) {
+		case NRF_ERROR_BUSY:
+			NRF_LOG_WARNING("NUS BUSY");
+			return;
+			break;
+
+		case NRF_ERROR_RESOURCES:
+			m_nus_cts = false;
+			break;
+
+		case NRF_ERROR_TIMEOUT:
+			return;
+			break;
+
+		case NRF_SUCCESS:
+		{
+			m_to_nus_data.length = 0;
+
+			if (!nrf_queue_is_empty(&m_to_nus_queue)) {
+				ret_code_t err_code = nrf_queue_pop(&m_to_nus_queue, &m_to_nus_data);
+				APP_ERROR_CHECK(err_code);
+			}
+		} break;
+
+		default:
+		{
+			NRF_LOG_WARNING("NUS unknown error: 0x%X MTU %u / %u", ret, m_to_nus_data.length, BLE_NUS_MAX_DATA_LEN);
+			return;
+		} break;
+		}
 	}
 
 }
@@ -313,6 +430,13 @@ static void services_init(void)
 {
 	uint32_t       err_code;
 	ble_nus_init_t nus_init;
+	nrf_ble_qwr_init_t qwr_init = {0};
+
+	// Initialize Queued Write Module.
+	qwr_init.error_handler = nrf_qwr_error_handler;
+
+	err_code = nrf_ble_qwr_init(&m_qwr, &qwr_init);
+	APP_ERROR_CHECK(err_code);
 
 	memset(&nus_init, 0, sizeof(nus_init));
 
@@ -406,14 +530,20 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
 	case BLE_GAP_EVT_CONNECTED:
 		NRF_LOG_INFO("BLE NUS connected");
 		usb_printf("Connected");
+		m_nus_connected = true;
+		m_nus_cts = true;
 		err_code = app_timer_stop(m_blink_ble);
 		APP_ERROR_CHECK(err_code);
 		bsp_board_led_on(LED_BLE_NUS_CONN);
 		m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+		err_code = nrf_ble_qwr_conn_handle_assign(&m_qwr, m_conn_handle);
+		APP_ERROR_CHECK(err_code);
 		break;
 
 	case BLE_GAP_EVT_DISCONNECTED:
 		NRF_LOG_INFO("BLE NUS disconnected");
+		m_nus_connected = false;
+		m_nus_cts = false;
 		// LED indication will be changed when advertising starts.
 		m_conn_handle = BLE_CONN_HANDLE_INVALID;
 		break;
@@ -499,6 +629,12 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
 			}
 		}
 	} break; // BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST
+
+	case BLE_GATTC_EVT_WRITE_CMD_TX_COMPLETE:
+		NRF_LOG_DEBUG("GATTC WRITE_CMD_TX Complete %u", m_nus_cts);
+		// clear to send more packets
+		m_nus_cts = true;
+		break;
 
 	case BLE_GATTS_EVT_HVN_TX_COMPLETE:
 	{
@@ -652,12 +788,12 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 	{
 	case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
 	{
-    	m_is_port_open = true;
-        /*Setup first transfer*/
-    	index = 0;
-        ret_code_t ret = app_usbd_cdc_acm_read(&m_app_cdc_acm,
-        		&m_cdc_data_array[index],
-                1);
+		m_is_port_open = true;
+		/*Setup first transfer*/
+		index = 0;
+		ret_code_t ret = app_usbd_cdc_acm_read(&m_app_cdc_acm,
+				&m_cdc_data_array[index],
+				1);
 		UNUSED_VARIABLE(ret);
 		ret = app_timer_stop(m_blink_cdc);
 		APP_ERROR_CHECK(ret);
@@ -668,8 +804,8 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 
 	case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
 		NRF_LOG_INFO("CDC ACM port closed");
-    	m_is_port_open = false;
-    	m_is_xfer_done = true;
+		m_is_port_open = false;
+		m_is_xfer_done = true;
 		if (m_usb_connected)
 		{
 			ret_code_t ret = app_timer_start(m_blink_cdc,
@@ -682,6 +818,7 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 	case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
 	{
 		m_is_xfer_done = true;
+		m_to_cdc_data.length = 0;
 
 		break;
 	}
@@ -695,51 +832,40 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 		{
 			if ((m_cdc_data_array[index - 1] == '\n') ||
 					(m_cdc_data_array[index - 1] == '\r') ||
-					(index >= (m_ble_nus_max_data_len)))
+					(index + 2 >= (m_ble_nus_max_data_len)))
 			{
 				if (index > 1)
 				{
-					NRF_LOG_DEBUG("Ready to send data over BLE NUS");
-					NRF_LOG_HEXDUMP_DEBUG(m_cdc_data_array, index);
-
-					do
+					uint16_t length = (uint16_t)index;
+					if (length + sizeof(ENDLINE_STRING) < BLE_NUS_MAX_DATA_LEN)
 					{
-						uint16_t length = (uint16_t)index;
-						if (length + sizeof(ENDLINE_STRING) < BLE_NUS_MAX_DATA_LEN)
-						{
-							memcpy(m_cdc_data_array + length, ENDLINE_STRING, sizeof(ENDLINE_STRING));
-							length += sizeof(ENDLINE_STRING);
-						}
-
-						if (m_conn_handle == BLE_CONN_HANDLE_INVALID) {
-
-							usb_printf("ERROR: BLE not connected");
-						} else {
-
-							ret = ble_nus_data_send(&m_nus,
-									(uint8_t *) m_cdc_data_array,
-									&length,
-									m_conn_handle);
-						}
-
-						if (ret == NRF_ERROR_NOT_FOUND)
-						{
-							NRF_LOG_INFO("BLE NUS unavailable, data received: %s", m_cdc_data_array);
-							break;
-						}
-
-						if (ret == NRF_ERROR_RESOURCES)
-						{
-							NRF_LOG_ERROR("BLE NUS Too many notifications queued.");
-							break;
-						}
-
-						if ((ret != NRF_ERROR_INVALID_STATE) && (ret != NRF_ERROR_BUSY))
-						{
-							APP_ERROR_CHECK(ret);
-						}
+						memcpy(m_cdc_data_array + length, ENDLINE_STRING, sizeof(ENDLINE_STRING));
+						length += sizeof(ENDLINE_STRING);
 					}
-					while (ret == NRF_ERROR_BUSY);
+
+					if (m_conn_handle == BLE_CONN_HANDLE_INVALID) {
+
+						usb_printf("ERROR: BLE not connected");
+					} else {
+
+						if (!nrf_queue_is_full(&m_to_nus_queue)) {
+
+							sNusXfer _xfer;
+
+							memset(&_xfer, 0, sizeof(_xfer));
+
+							// get data
+							_xfer.length = length;
+							memcpy(_xfer.p_xfer_str, m_cdc_data_array, length);
+
+							// store it in a queue
+							ret_code_t err_code = nrf_queue_push(&m_to_nus_queue, &_xfer);
+							APP_ERROR_CHECK(err_code);
+
+						}
+
+					}
+
 				}
 
 				index = 0;
@@ -846,6 +972,10 @@ int main(void)
 	log_init();
 	timers_init();
 
+#if APP_SCHEDULER_ENABLED
+	APP_SCHED_INIT(SCHED_MAX_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
+#endif
+
 	buttons_leds_init();
 
 	app_usbd_serial_num_generate();
@@ -880,7 +1010,15 @@ int main(void)
 	{
 		usb_cdc_tasks();
 
+		cdc_process();
+
+		nus_process();
+
 		idle_state_handle();
+
+#if APP_SCHEDULER_ENABLED
+		app_sched_execute();
+#endif
 	}
 }
 
