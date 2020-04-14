db1efc1c36216fa075f9870d785750910d9b9266
vincent290587 <vincent.golle@gmail.com>
2020-04-14 13:11:36 +0200

    Baro correction added to log

--- source/sd/sd_functions.cpp
+++ source/sd/sd_functions.cpp
@@ -622,12 +622,12 @@ void sd_save_pos_buffer(SAttTime* att, uint16_t nb_pos) {
 		int to_wr = snprintf(g_bufferWrite, sizeof(g_bufferWrite),
 				"%f;%f;%.2f;%lu;"
 				"%d;%u;%lu;"
-				"%.3f;%.3f;%.3f;"
+				"%.3f;%.3f;%.3f;%.2f;"
 				"%.1f;%.2f;%.2f;%.2f;"
 				"\r\n",
 				att[i].loc.lat, att[i].loc.lon, att[i].loc.alt, att[i].date.secj,
 				att[i].sensors.pwr, att[i].sensors.bpm, att[i].sensors.cadence,
-				att[i].alti.alpha_bar, att[i].alti.alpha_zero, att[i].alti.baro_ele,
+				att[i].alti.alpha_bar, att[i].alti.alpha_zero, att[i].alti.baro_ele, att[i].alti.baro_corr,
 				att[i].alti.climb, att[i].alti.filt_ele, att[i].alti.gps_ele, att[i].alti.vit_asc
 				);
 
